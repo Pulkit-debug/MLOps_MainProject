3534dd75a39e6f1ee58d2441001e4ec49a04cbcc
Pulkit Verma <vpulkit567@gmail.com>
2022-10-02 15:22:27 +0530

    TALT #4

--- houses.py
+++ houses.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-# Testing it after a long time
+# Testing project after a long time
 # # Using MobileNet for our House Classifer
 # 
 # ### Loading the MobileNet Model
